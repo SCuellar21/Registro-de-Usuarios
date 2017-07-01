40ca724d8c80c92e065bf68cff5c45393aa447fb
Sergio Cuéllar <s.cuellar.21s@gmail.com>
2017-07-01 18:25:54 -0500

    v1.0
    Final aunque se tiene que corregir el ENTER

---
+++ tercer parcial/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace tercer_parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lMensaje = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lNombre = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(126, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Contraseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(136, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cuenta";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.UseSystemPasswordChar = true;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown_1);
+            // 
+            // lMensaje
+            // 
+            this.lMensaje.AutoSize = true;
+            this.lMensaje.ForeColor = System.Drawing.SystemColors.Control;
+            this.lMensaje.Location = new System.Drawing.Point(126, 200);
+            this.lMensaje.Name = "lMensaje";
+            this.lMensaje.Size = new System.Drawing.Size(60, 17);
+            this.lMensaje.TabIndex = 8;
+            this.lMensaje.Text = "Bienvenido";
+            this.lMensaje.UseCompatibleTextRendering = true;
+            this.lMensaje.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(60, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(182, 185);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // lNombre
+            // 
+            this.lNombre.AutoSize = true;
+            this.lNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNombre.ForeColor = System.Drawing.SystemColors.Control;
+            this.lNombre.Location = new System.Drawing.Point(126, 217);
+            this.lNombre.Name = "lNombre";
+            this.lNombre.Size = new System.Drawing.Size(57, 20);
+            this.lNombre.TabIndex = 3;
+            this.lNombre.Text = "label5";
+            this.lNombre.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(296, 274);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lMensaje);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lMensaje;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ tercer parcial/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace tercer_parcial
+{
+    public partial class Form1 : Form
+    {
+        public int num_usuarios, num_registros;
+        public struct Usuarios
+        { 
+            public int cuenta;
+            public string nombre;
+            public string contraseña;
+            public string imagen;
+        }
+        public Usuarios[] usuarios;
+        public struct Registros
+        {
+            public int cuenta;
+            public string nombre;
+            public DateTime entrada;
+            public DateTime salida;
+        }
+        public Registros[] registros = new Registros[10000];
+        public string[] temp = new string[4];
+        public int N;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ///Lee y ordena los datos de los usuarios
+            FileStream stream = new FileStream("Usuarios.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            for (int i = 0; reader.Peek() > -1; i++)
+            {
+                reader.ReadLine();
+                num_usuarios++;
+            }
+
+            num_usuarios = num_usuarios - 1;
+            usuarios = new Usuarios[num_usuarios];
+            stream.Seek(0, SeekOrigin.Begin);
+            N = Convert.ToInt32(reader.ReadLine());
+            for (int i = 0; reader.Peek() > -1; i++)
+            {
+                string Linea = reader.ReadLine();
+                temp = Linea.Split('-');
+
+                if (temp[0] == null)
+                {
+                    break;
+                }
+
+                usuarios[i].cuenta = Convert.ToInt32(temp[0]);
+                usuarios[i].nombre = temp[1];
+                usuarios[i].contraseña = temp[2];
+                usuarios[i].imagen = temp[3];
+            }
+
+            stream.Close();
+            reader.Close();
+        }
+
+        public DateTime origen = new DateTime(0001, 01, 01, 00, 00, 00);
+        private void textBox2_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                for (int i = 0; i < num_usuarios; i++)
+                {
+                    if (Convert.ToInt32(textBox1.Text) == usuarios[i].cuenta && textBox2.Text == usuarios[i].contraseña)  //revisa que el usuario y la contraseña sean correctos
+                    {
+                        for (int j = 0; j <= num_registros; j++)
+                        {
+                            if (registros[j].cuenta != 0)  //si el registro esta vacio, escribe uno nuevo
+                            {
+                                if (registros[j].cuenta == usuarios[i].cuenta && 0 == DateTime.Compare(registros[j].salida, origen))  //si el usuario YA esta registrado pero NO HA SALIDO
+                                {
+                                    registros[j].salida = DateTime.Now;
+
+                                    pictureBox2.Image = Image.FromFile("img/" + usuarios[i].imagen + ".jpg");
+                                    lNombre.Text = usuarios[i].nombre;
+                                    lMensaje.Text = "Adios";
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                registros[num_registros].cuenta = usuarios[i].cuenta;
+                                registros[num_registros].nombre = usuarios[i].nombre;
+                                registros[num_registros].entrada = DateTime.Now;
+                                num_registros++;
+
+                                pictureBox2.Image = Image.FromFile("img/" + usuarios[i].imagen + ".jpg");
+                                lNombre.Text = usuarios[i].nombre;
+                                lMensaje.Text = "Bienvenido";
+                                break;
+                            }
+                        }
+                        textBox1.Clear();
+                        textBox2.Clear();
+                        label1.Visible = false;
+                        label2.Visible = false;
+                        textBox1.Visible = false;
+                        textBox2.Visible = false;
+                        pictureBox2.Visible = true;
+                        lMensaje.Visible = true;
+                        lNombre.Visible = true;
+                        timer1.Interval = 2500;
+                        timer1.Enabled = true;
+                        return;
+                    }
+                }
+                MessageBox.Show("Número de cuenta o contraseña incorrectos.");
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            lMensaje.Visible = false;
+            lNombre.Visible = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            //textBox1.Focus();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FileStream stream = new FileStream("Registro" + (N + 1) + ".txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            for (int i = 0, x = 0; i < 10000 && x < num_registros; i++)
+            {
+                if (registros[i].cuenta != 0)
+                {
+                    string linea = registros[i].cuenta + "-" + registros[i].nombre + "-" + registros[i].entrada + "-" + registros[i].salida;
+                    writer.WriteLine(linea);
+                    x++;
+                }
+            }
+            writer.Close();
+            stream.Close();
+
+
+            /*
+            *   Actualiza es documento USUARIOS para contar el numero de registros "N" realizados
+            */
+            N++;
+            stream = new FileStream("Usuarios.txt", FileMode.Create, FileAccess.Write);
+            writer = new StreamWriter(stream);
+            writer.WriteLine(N);
+            for (int i = 0; i < num_usuarios; i++)
+            {
+                if (usuarios[i].cuenta != 0)
+                {
+                    string linea = usuarios[i].cuenta + "-" + usuarios[i].nombre + "-" + usuarios[i].contraseña + "-" + usuarios[i].imagen;
+                    writer.WriteLine(linea);
+                }
+            }
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
